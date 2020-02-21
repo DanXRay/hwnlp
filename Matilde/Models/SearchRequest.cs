38ab6303ecd22e62d1c7da5e0b20e1c9f6d62231
Dan Ray <dray@healthwise.org>
2020-02-21 15:19:32 -0700

    fix?

--- Matilde/Models/SearchRequest.cs
+++ Matilde/Models/SearchRequest.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
 using System.Net;
 using System.Text;
 
